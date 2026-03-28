aaf9e4559aab7bb98c630f0d0c9cc17ba217f9e6
nikhil-tinvio <nikhil.patil@tinvio.com>
2026-03-29 02:28:50 +0530

    next

--- app.py
+++ app.py
@@ -10,6 +10,10 @@ app = FastAPI()
 def read_root():
     return {"message": "Hello World"}
 
+@app.get('/hello')
+def read_hello():
+    return {"message": "Hello World"}
+
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
